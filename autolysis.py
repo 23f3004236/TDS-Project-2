b5e63f1987165675259bc4d3364953f4cc87012b
23f3004236 <23f3004236@ds.study.iitm.ac.in>
2024-12-16 13:21:04 +0530

    Update autolysis.py

--- autolysis.py
+++ autolysis.py
@@ -1,17 +1,15 @@
-# /// script
-# requires-python = ">=3.11"
-# dependencies = [
-#   "httpx",
-#   "pandas",
-#   "matplotlib",
-#   "seaborn",
-#   "openai",
-#   "chardet",
-#   "scipy",
-#   "numpy",
-#   "joypy",
-# ]
-# ///
+# Script Information
+# Requires Python >=3.11
+# Dependencies:
+#   - httpx
+#   - pandas
+#   - matplotlib
+#   - seaborn
+#   - openai
+#   - chardet
+#   - scipy
+#   - numpy
+#   - joypy
 
 import os
 import sys
